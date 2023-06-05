da0efb78925c4ab2b066011204ca94633d7bd926
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-06-05 18:18:31 +0300

    hmwrk1 creating funcs and testsfor

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+@pytest.fixture()
+def item():
+    return Item("Яблоко", 2, 3)
+
+def test_Item_init(item):
+    assert item.name == "Яблоко"
+    assert item.price == 2
+    assert item.quantity == 3
+
+def test_Item_calculate_total_price(item):
+    assert item.calculate_total_price() == 6
+
+def test_apply_discount(item):
+    assert item.apply_discount() == 2
