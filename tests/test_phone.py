efe31dbed728f14cc8f58566efaa6ddb0e7cc7c6
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-06-25 18:40:18 +0300

    changes for class Phone and test for

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,4 +1,4 @@
-from src.phone import Phone
+from src.phone import Phone, Item
 
 def test_init():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
@@ -10,3 +10,10 @@ def test_init():
 def test_repr():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+
+def test_add_phone():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
+    assert phone1 + 2 == None
